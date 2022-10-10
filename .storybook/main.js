87952122d0882999d36629e84b18dddb75ec1351
tnyo43 <tnyo43@gmail.com>
2022-10-10 15:53:48 +0900

    update storybook with vite builder

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,7 @@
+const react = require('@vitejs/plugin-react');
+
 module.exports = {
-  stories: ['../**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -7,6 +9,37 @@ module.exports = {
   ],
   framework: '@storybook/react',
   core: {
-    builder: '@storybook/builder-webpack5'
+    builder: '@storybook/builder-vite'
+  },
+  features: {
+    storyStoreV7: true
+  },
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes('vite:react'))
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/node_modules/],
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin']
+        }
+      })
+    );
+
+    config.esbuild = {
+      logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    };
+
+    return {
+      ...config,
+      define: {
+        ...config.define,
+        global: 'window'
+      }
+    };
   }
 };
