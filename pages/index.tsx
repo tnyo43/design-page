3f41d1ff26b40ae44ff348f3bcf79bb543a0eb2a
Tomoya Kashifuku <tnyo43@gmail.com>
2022-11-18 02:49:50 +0900

    add babel plugin for svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next';
 
 import { Button } from '../src/components/Button';
+import CreateIcon from '../src/assets/create.svg';
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>Desing Page</h1>
-      <Button theme='primary' onClick={() => console.log('hello')}>
+      <Button theme="primary" onClick={() => console.log('hello')}>
         Hello
+      </Button>
+      <Button theme="primary">
+        <CreateIcon />
+        Create
       </Button>
     </div>
   );
