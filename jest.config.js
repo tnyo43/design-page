fa120fa2e7f45ba62efe9fc772ff4674714de77a
Tomoya Kashifuku <tnyo43@gmail.com>
2022-11-18 12:37:45 +0900

    update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,14 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: [
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
-    '**/__tests__/**/*.+(ts|tsx|js)'
+    '**/__tests__/**/*.+(ts|tsx|js)',
   ],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: './tsconfig.jest.json'
-      }
-    ]
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  moduleNameMapper: {
+    '~/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
