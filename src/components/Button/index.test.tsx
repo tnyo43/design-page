cbe967d8f5d1d468e58f86a6000e83099fcf84c5
Tomoya Kashifuku <tnyo43@gmail.com>
2022-11-18 19:19:51 +0900

    Revert "check with red test"
    This reverts commit 89e19d7e1c654cbd653428250bc1edc7385bf3d6.

--- src/components/Button/index.test.tsx
+++ src/components/Button/index.test.tsx
@@ -7,7 +7,7 @@ describe('Button', () => {
   test('唯一の role として button を持つ', () => {
     const { container } = render(<Button>ボタン</Button>);
 
-    expect(getRoles(container).length).toBe(100);
+    expect(getRoles(container).length).toBe(1);
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
